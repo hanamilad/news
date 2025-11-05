3b86263d1ac2380f76d9f627d5e38b397737f6e6
hana_milad <89672587+hanamilad@users.noreply.github.com>
2025-11-05 11:35:09 +0200

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\CheckPermission;
 use App\Http\Middleware\SetLocaleFromHeader;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
