9163daf33ea0de4a6963edaa8319f8fab9d2351b
hana_milad <89672587+hanamilad@users.noreply.github.com>
2025-11-20 16:29:34 +0200

    .

--- app/Http/Requests/Video/VideoRequest.php
+++ app/Http/Requests/Video/VideoRequest.php
@@ -16,7 +16,7 @@ class VideoRequest extends FormRequest
         return [
             'description' => 'required|array',
             'description.*' => 'nullable|string',
-            'video_path' => 'required|string|max:255',
+            'video_path' => 'nullable|string|max:255',
             'type' => 'required|string|in:short,long',
             'is_active' => 'nullable|boolean',
             'publish_date' => 'nullable|date',
@@ -29,7 +29,6 @@ class VideoRequest extends FormRequest
             'description.required' => 'وصف الفيديو  مطلوب.',
             'description.array' => 'يجب إرسال الاسم كمجموعة من الترجمات (مثلاً ar, en).',
             'description.*.string' => 'كل ترجمة يجب أن تكون نص.',
-            'video_path.required' => 'مسار الفديدو مطلوب.',
             'video_path.string'   => 'مسار الفيديو يجب ان يكون نصا.',
             'video_path.max'      => 'مسار الفيديو طويل جدا .',
         ];
