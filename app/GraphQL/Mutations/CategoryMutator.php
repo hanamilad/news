3cba66db889d8edb17cdd46ad41685b5c75ac9c8
hana_milad <89672587+hanamilad@users.noreply.github.com>
2025-11-23 11:05:35 +0200

    Category

--- app/GraphQL/Mutations/CategoryMutator.php
+++ app/GraphQL/Mutations/CategoryMutator.php
@@ -5,6 +5,8 @@ namespace App\GraphQL\Mutations;
 use App\Services\Category\CategoryService;
 use App\Http\Requests\Category\CategoryRequest;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\DB;
+
 
 class CategoryMutator
 {
@@ -12,25 +14,60 @@ class CategoryMutator
 
     public function create($_, array $args)
     {
-        $input = $args['input'] ?? [];
-        $validator = validator($input, (new CategoryRequest())->rules());
-        if ($validator->fails()) {
-            throw new ValidationException($validator);
+        $inputs = $args['input'] ?? [];
+        $results = [];
+        DB::beginTransaction();
+        try {
+            foreach ($inputs as $input) {
+                // Validate each category separately
+                $validator = validator($input, (new CategoryRequest())->rules());
+                if ($validator->fails()) {
+                    throw new ValidationException($validator);
+                }
+
+                $results[] = $this->service->create($input);
+            }
+            DB::commit();
+            return $results;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
         }
-        return $this->service->create($input);
     }
+
 
     public function update($_, array $args)
     {
-        $id = (int)$args['id'];
-        $input = $args['input'] ?? [];
-        $validator = validator($input, (new CategoryRequest())->rules());
-        if ($validator->fails()) {
-            throw new ValidationException($validator);
-        }
+        $inputs = $args['input'] ?? [];
+        $updated = [];
 
-        return $this->service->update($id, $input);
+        DB::beginTransaction();
+
+        try {
+            foreach ($inputs as $item) {
+                if (!isset($item['id']) || !isset($item['data'])) {
+                    throw new \Exception("id and data fields are required for each update item");
+                }
+
+                $id = (int) $item['id'];
+                $data = $item['data'];
+
+                $validator = validator($data, (new CategoryRequest())->rules());
+                if ($validator->fails()) {
+                    throw new ValidationException($validator);
+                }
+
+                $updated[] = $this->service->update($id, $data);
+            }
+
+            DB::commit();
+            return $updated;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
     }
+
 
     public function delete($_, array $args)
     {
