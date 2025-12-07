c51eb6192508e5bb82e227a922fe2b8588077983
hana_milad <89672587+hanamilad@users.noreply.github.com>
2025-12-07 11:47:35 +0200

    Update tenant.php

--- routes/tenant.php
+++ routes/tenant.php
@@ -22,8 +22,4 @@ Route::middleware([
     'web',
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
-])->group(function () {
-    Route::get('/', function () {
-        return 'This is your multi-tenant application. The id of the current tenant is '.tenant('id');
-    });
-});
+])->group(function () {});
