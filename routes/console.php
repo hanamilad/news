58d0b4c39fde94b0ca320d6b7c134f4529af6916
hana_milad <89672587+hanamilad@users.noreply.github.com>
2026-01-05 16:54:32 +0200

    feat: add `inspire` Artisan command and scheduled task to de-prioritize old urgent news.

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    \App\Models\News::query()
+        ->where('is_urgent', true)
+        ->where('publish_date', '<=', now()->subHours(2))
+        ->update(['is_urgent' => false]);
+})->everyMinute();
