21abece04fba1992207a22d7ab06659c3273a84f
hana_milad <89672587+hanamilad@users.noreply.github.com>
2026-01-06 10:31:23 +0200

    feat: Add `inspire` Artisan command and scheduled task to demote old urgent news.

--- routes/console.php
+++ routes/console.php
@@ -1,16 +1,17 @@
 <?php
 
+use App\Models\News;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
+use Stancl\Tenancy\Database\TenantScope;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 Schedule::call(function () {
-    \App\Models\News::query()
+    $affected =News::withoutGlobalScope(TenantScope::class)
         ->where('is_urgent', true)
         ->where('publish_date', '<=', now()->subHours(2))
         ->update(['is_urgent' => false]);
