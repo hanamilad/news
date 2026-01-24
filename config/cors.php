195fcf3f234cc68ce468b34f15c0d83017ea6526
hana_milad <89672587+hanamilad@users.noreply.github.com>
2026-01-24 14:49:31 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://soot-elhaq-one.vercel.app/',
+        'https://soot-elhaq-one.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
